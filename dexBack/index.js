9d241d89fa63865a6b65dbaa30b8f9e61761a3b8
DividexCollin <collin@dividex.org>
2023-03-06 20:27:29 -0500

    2023.03.06

--- dexBack/index.js
+++ dexBack/index.js
@@ -9,6 +9,23 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/tokenPrice", async (req, res) => {
+  
+  const {query} = req;
+
+  const responseOne = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressOne //need to include which chain as a parameter if not Ethereum
+  })
+
+  const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressTwo //need to include which chain as a parameter if not Ethereum
+  })
+
+  const usdPrices = {
+    tokenOne: responseOne.raw.usdPrice,
+    tokenTwo: responseTwo.raw.usdPrice,
+    ratio: responseOne.raw.usdPrice/responseTwo.raw.usdPrice
+  }
+  
   return res.status(200).json(usdPrices);
 });
 
